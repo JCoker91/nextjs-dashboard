cf84d7c222a329e58a87ee76b376cb151813bb25
Jake Coker <jake.coker91@gmail.com>
2024-10-14 15:43:23 -0700

    replaced useActionState with useFormState

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -9,12 +9,13 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { useActionState } from 'react';
+// import { useActionState } from 'react';
+import { useFormState } from 'react-dom';
 import { createInvoice, State } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = {message: null, errors: {}};
-  const [state, formAction] = useActionState(createInvoice, initialState);
+  const [state, formAction] = useFormState(createInvoice, initialState);
   return (
     <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
